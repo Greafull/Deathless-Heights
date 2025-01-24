fe50748a7445daa4b84e48cd755fcffaf2b05732
Greafull <micmicgames21@gmail.com>
2025-01-23 21:26:42 -0500

    added skybox

---
+++ core/src/main/java/me/greaful/system/SkyboxShader.kt
@@ -0,0 +1,46 @@
+package me.greaful.system
+
+import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.Cubemap
+import com.badlogic.gdx.graphics.g3d.Renderable
+import com.badlogic.gdx.graphics.g3d.Shader
+import com.badlogic.gdx.graphics.g3d.utils.RenderContext
+import com.badlogic.gdx.graphics.glutils.ShaderProgram
+
+class SkyboxShader(
+    private val shaderProgram: ShaderProgram,
+    private var camera: Camera?,
+    private val skyboxCubemap: Cubemap
+) : Shader {
+
+    override fun dispose() {
+        shaderProgram.dispose()
+    }
+
+    override fun end() {
+        shaderProgram.end()
+    }
+
+    override fun canRender(instance: Renderable?): Boolean {
+        return true
+    }
+
+    override fun render(renderable: Renderable?) {
+        renderable?.meshPart?.render(shaderProgram)
+    }
+
+    override fun init() {
+  //not().
+        TODO("Not needed")
+    }
+
+    override fun begin(camera: Camera?, context: RenderContext?) {
+        this.camera = camera
+        shaderProgram.begin()
+        shaderProgram.setUniformMatrix("u_projTrans", camera?.combined)
+    }
+
+    override fun compareTo(other: Shader?): Int {
+        return 0
+    }
+}
